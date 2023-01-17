23c3644964dbe276741538db1e91b2158b96820a
Danny Yan <linda502@gmail.com>
2023-01-16 18:55:20 -0800

    eslint

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   env: {
     allCategoriesApiUrl: 'http://47.74.37.101:3001/categoriesAll',
