e2980c73af1ea7e1175fb4c8c1b43f47bfab8e1d
Danny Yan <linda502@gmail.com>
2023-03-22 11:59:19 -0700

    update the backend server ip

--- next.config.js
+++ next.config.js
@@ -7,12 +7,12 @@ const nextConfig = {
   },
   reactStrictMode: true,
   env: {
-    allCategoriesApiUrl: 'http://47.74.37.101:3001/categoriesAll',
-    allCitiesApiUrl: 'http://47.74.37.101:3001/citiesAll',
-    allProvincesApiUrl: 'http://47.74.37.101:3001/provincesAll',
-    insertOrderApiUrl: 'http://47.74.37.101:3001/insertOrder',
-    getOrderDetailsApiUrl: 'http://47.74.37.101:3001/orderByOrderId?orderId=',
-    insertServiceProviderUrl: 'http://47.74.37.101:3001/insertProvider',
+    allCategoriesApiUrl: 'http://localhost:3001/categoriesAll',
+    allCitiesApiUrl: 'http://localhost:3001/citiesAll',
+    allProvincesApiUrl: 'http://localhost:3001/provincesAll',
+    insertOrderApiUrl: 'http://localhost:3001/insertOrder',
+    getOrderDetailsApiUrl: 'http://localhost:3001/orderByOrderId?orderId=',
+    insertServiceProviderUrl: 'http://localhost:3001/insertProvider',
     TWILIO_ACCOUNT_SID:"AC3e19c2e32c6c3586798e7a42d473d37b",
     TWILIO_AUTH_TOKEN:"ecec5c6dc4a3f9b1f5fce99e02aee9f2",
   }
