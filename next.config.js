ea9228920498182059c453958ed01f63a4ae2606
Danny Yan <linda502@gmail.com>
2023-03-30 21:23:07 -0700

    change http to https

--- next.config.js
+++ next.config.js
@@ -7,12 +7,12 @@ const nextConfig = {
   },
   reactStrictMode: true,
   env: {
-    allCategoriesApiUrl: 'http://153.92.214.244:3001/categoriesAll',
-    allCitiesApiUrl: 'http://153.92.214.244:3001/citiesAll',
-    allProvincesApiUrl: 'http://153.92.214.244:3001/provincesAll',
-    insertOrderApiUrl: 'http://153.92.214.244:3001/insertOrder',
-    getOrderDetailsApiUrl: 'http://153.92.214.244:3001/orderByOrderId?orderId=',
-    insertServiceProviderUrl: 'http://153.92.214.244:3001/insertProvider',
+    allCategoriesApiUrl: 'https://153.92.214.244:3001/categoriesAll',
+    allCitiesApiUrl: 'https://153.92.214.244:3001/citiesAll',
+    allProvincesApiUrl: 'https://153.92.214.244:3001/provincesAll',
+    insertOrderApiUrl: 'https://153.92.214.244:3001/insertOrder',
+    getOrderDetailsApiUrl: 'https://153.92.214.244:3001/orderByOrderId?orderId=',
+    insertServiceProviderUrl: 'https://153.92.214.244:3001/insertProvider',
     TWILIO_ACCOUNT_SID:"AC3e19c2e32c6c3586798e7a42d473d37b",
     TWILIO_AUTH_TOKEN:"ecec5c6dc4a3f9b1f5fce99e02aee9f2",
   }
