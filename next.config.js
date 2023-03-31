158e97cfe72bca5bfdaffbc58a1519e9edf31cfd
Danny Yan <linda502@gmail.com>
2023-03-30 21:16:08 -0700

    change to server ip for api

--- next.config.js
+++ next.config.js
@@ -7,12 +7,12 @@ const nextConfig = {
   },
   reactStrictMode: true,
   env: {
-    allCategoriesApiUrl: 'http://localhost:3001/categoriesAll',
-    allCitiesApiUrl: 'http://localhost:3001/citiesAll',
-    allProvincesApiUrl: 'http://localhost:3001/provincesAll',
-    insertOrderApiUrl: 'http://localhost:3001/insertOrder',
-    getOrderDetailsApiUrl: 'http://localhost:3001/orderByOrderId?orderId=',
-    insertServiceProviderUrl: 'http://localhost:3001/insertProvider',
+    allCategoriesApiUrl: 'http://153.92.214.244:3001/categoriesAll',
+    allCitiesApiUrl: 'http://153.92.214.244:3001/citiesAll',
+    allProvincesApiUrl: 'http://153.92.214.244:3001/provincesAll',
+    insertOrderApiUrl: 'http://153.92.214.244:3001/insertOrder',
+    getOrderDetailsApiUrl: 'http://153.92.214.244:3001/orderByOrderId?orderId=',
+    insertServiceProviderUrl: 'http://153.92.214.244:3001/insertProvider',
     TWILIO_ACCOUNT_SID:"AC3e19c2e32c6c3586798e7a42d473d37b",
     TWILIO_AUTH_TOKEN:"ecec5c6dc4a3f9b1f5fce99e02aee9f2",
   }
